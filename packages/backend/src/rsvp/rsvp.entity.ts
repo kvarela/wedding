637237edefd4aeaf60c9d5afbfbee5d4527f83dc
kvarela <karim@metalink.com>
2026-03-01 15:16:20 -0800

    unique phone, email, and name

--- packages/backend/src/rsvp/rsvp.entity.ts
+++ packages/backend/src/rsvp/rsvp.entity.ts
@@ -6,14 +6,14 @@ export class Rsvp {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, unique: true })
   name: string
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, unique: true })
   email: string
 
-  @Column({ type: 'varchar', length: 50, nullable: true })
-  phone: string | null
+  @Column({ type: 'varchar', length: 50, unique: true })
+  phone: string
 
   @Column({ type: 'integer' })
   numGuests: number
